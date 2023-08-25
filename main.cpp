52db087d2c395d9d2c56f5be53f5d89206492164
lyy <1421573381@qq.com>
2023-08-25 14:43:36 +0800

    up

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <thread>
+#include <sys/eventfd.h>
+#include <unistd.h>
+
+void threadFunction(int threadId) {
+    std::cout << "Thread " << threadId << " started." << std::endl;
+
+    // Create eventfd file descriptor
+    int efd = eventfd(0, EFD_NONBLOCK);
+    if (efd == -1) {
+        perror("eventfd");
+        return;
+    }
+
+    std::cout << "ThreadId: " << threadId << ", efd value:"  << efd << " finished." << std::endl;
+}
 
 int main() {
-    std::cout << "hello , websocket" << std::endl;
+    std::thread threads[5];
+
+    for (int i = 0; i < 5; ++i) {
+        threads[i] = std::thread(threadFunction, i);
+    }
+
+    for (int i = 0; i < 5; ++i) {
+        threads[i].join();
+    }
+
     return 0;
 }
